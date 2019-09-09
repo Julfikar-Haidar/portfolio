266bc3beccf95e224ad496be2dadbe92879f91f7
Julfikar Haidar <44232776+Julfikar-Haidar@users.noreply.github.com>
2019-09-09 21:10:04 +0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -512,7 +512,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Freelancer", "Full Stack Web Developer"],
+			strings: ["Full Stack Web Developer"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
